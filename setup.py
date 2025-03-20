ac2b81d61010cb85121428d00dd2bf73b4d1b744
Mahesh Pathake <2000109649@hexaware.com>
2025-03-20 21:56:00 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Mahesh Pathake',
+    author_email= 'maheshpathake4ai@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
